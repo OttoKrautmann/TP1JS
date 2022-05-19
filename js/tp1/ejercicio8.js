bcf2e0448939b15038209fd8aa7c20aa69648f53
Otto Krautmann <krautmann.otto@gmail.com>
2022-05-19 09:24:45 -0300

    hice el ejercicio 8

---
+++ js/tp1/ejercicio8.js
@@ -0,0 +1,8 @@
+var numero = parseInt(prompt("ingrese un numero"));
+var numero = numero%2
+if(numero = 0){
+    document.write("el numero si es divisible en 2")
+}
+else{
+    document.write("el numero no es divisible en 2")
+}
