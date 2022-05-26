c8443657c6157bb136a7f51568ff0f79b0eb492b
Otto Krautmann <krautmann.otto@gmail.com>
2022-05-25 22:46:42 -0300

    corregi el 9

--- js/tp1/ejercicio9.js
+++ js/tp1/ejercicio9.js
@@ -1,1 +1,8 @@
-let palabra = prompt("ingrese una palabra de 5 letras");
+let palabra = prompt("ingrese una frase");
+palabra = palabra.toLowerCase()
+console.log(palabra.length)
+for(let indice = 0; indice < palabra.length; indice ++){
+    if(palabra.charAt(indice) === "a" || palabra.charAt(indice) === "e" || palabra.charAt(indice) === "i" || palabra.charAt(indice) === "o" || palabra.charAt(indice) === "u"){
+        document.write(palabra.charAt(indice))
+    }
+}
